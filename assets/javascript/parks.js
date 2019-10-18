a0acf8c9945dabc73b4bd7b6dd4f5ee7fdffad22
Samantha Lynnes <samanthalynnes522@gmailcom>
2019-10-18 10:36:05 -0500

    added on click event for button

--- assets/javascript/parks.js
+++ assets/javascript/parks.js
@@ -1,21 +1,23 @@
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   
-    // This .on("click") function will trigger the AJAX Call
-    $("#user-state-input").on("click", function(event) {
+  
+  // This .on("click") function will trigger the AJAX Call
+    $("#btn").on("click", function(event) {
 
         // event.preventDefault() can be used to prevent an event's default behavior.
         // Here, it prevents the submit button from trying to submit a form when clicked
         event.preventDefault();
 
         // Here we grab the text from the input box
-        var stateInput = $("#user-state-input").val();
+        var stateNameAbbr = $("#states :selected").attr("abbr");
+        console.log(stateNameAbbr);
+        //var stateName = $("#states :selected").val();
 
     // This is our API key
     var APIKey = "rNNmbbVGYhG0JXXavcAsDZoDUxLVy97nKeTT9pyj";
 
     // Here we are building the URL we need to query the database
-    var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateInput + "&api_key=" + APIKey;
+    var queryURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateNameAbbr + "&api_key=" + APIKey;
 
     // Here we run our AJAX call to the National Parks API
     $.ajax({
@@ -31,7 +33,7 @@
         // Log the resulting object
         console.log(response);
 
-        $("#state-output").text(response);
+        //$("#state-output").text(response);
 
         // Transfer content to HTML
         // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
